3ca7fa0c5b223525c3f4fecf6838ad2c941d9391
Raaaaaaaay86 <mirror46258@gmail.com>
2020-11-24 20:29:38 +0800

    cache and SW registering

---
+++ Cache_RegisterSW/public/sw.js
@@ -0,0 +1,57 @@
+const CACHE_STATIC_NAME = 'static-v1.0.2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1.0.2';
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installing...', event);
+
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+    .then((cache) => {
+      console.log('[Service Worker] Pre-caching App Shell...')
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/css/app.css',
+        '/src/css/main.css',
+        '/src/js/main.js',
+        '/src/js/material.min.js',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activating...', event);
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing Old Cache...');
+            return caches.delete(key);
+          }
+        }))
+      })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) return response;
+        return fetch(event.request)
+          .then((res) => {
+            return caches.open(CACHE_DYNAMIC_NAME)
+              .then((cache) => {
+                cache.put(event.request.url, res);
+                return res;
+              })
+          })
+          .catch((err) => console.log(err));
+      })
+  )
+});
