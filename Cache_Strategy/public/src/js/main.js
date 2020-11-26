51e7a00551da09121d61474964631693b1fc3dfb
Raaaaaaaay86 <mirror46258@gmail.com>
2020-11-26 19:04:05 +0800

    快取策略範例

--- Cache_Strategy/public/src/js/main.js
+++ Cache_Strategy/public/src/js/main.js
@@ -18,19 +18,35 @@ button.addEventListener('click', function(event) {
   }
 });
 
+const url = 'https://httpbin.org/ip';
+let networkResponseRecived = false;
+
 fetch('https://httpbin.org/ip')
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
-    console.log(data.origin);
+    console.log(data.origin, 'From Network', data);
+    networkResponseRecived = true;
     box.style.height = (data.origin.substr(0, 2) * 5) + 'px';
   });
 
-// 1) Identify the strategy we currently use in the Service Worker (for caching)
-// 2) Replace it with a "Network only" strategy => Clear Storage (in Dev Tools), reload & try using your app offline
-// 3) Replace it with a "Cache only" strategy => Clear Storage (in Dev Tools), reload & try using your app offline
-// 4) Replace it with "Network, cache fallback" strategy =>  => Clear Storage (in Dev Tools), reload & try using your app offline
+if ('caches' in window) {
+  caches.match(url)
+    .then((response) => {
+      if (response) return response.json();
+    })
+    .then((data) => {
+      console.log('From Cache', data);
+      if (!networkResponseRecived) {
+        box.style.height = (data.origin.substr(0, 2) * 20) + 'px';
+      }
+    })
+}
+//ok 1) Identify the strategy we currently use in the Service Worker (for caching)
+//ok 2) Replace it with a "Network only" strategy => Clear Storage (in Dev Tools), reload & try using your app offline
+//ok 3) Replace it with a "Cache only" strategy => Clear Storage (in Dev Tools), reload & try using your app offline
+//ok 4) Replace it with "Network, cache fallback" strategy =>  => Clear Storage (in Dev Tools), reload & try using your app offline
 // 5) Replace it with a "Cache, then network" strategy => Clear Storage (in Dev Tools), reload & try using your app offline
 // 6) Add "Routing"/ URL Parsing to pick the right strategies: Try to implement "Cache, then network", "Cache with network fallback" and "Cache only" (all of these, with appropriate URL selection)
 
--- Cache_Strategy/public/sw.js
+++ Cache_Strategy/public/sw.js
@@ -1,148 +1,128 @@
-var CACHE_STATIC_NAME = 'static-v13';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
-var STATIC_FILES = [
+
+var CACHE_STATIC_NAME = 'static-v2';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const url = 'https://httpbin.org/ip';
+const STATIC_FILES = [
   '/',
   '/index.html',
-  '/offline.html',
-  '/src/js/app.js',
-  '/src/js/feed.js',
-  '/src/js/promise.js',
-  '/src/js/fetch.js',
-  '/src/js/material.min.js',
   '/src/css/app.css',
-  '/src/css/feed.css',
-  '/src/images/main-image.jpg',
+  '/src/css/main.css',
+  '/src/js/main.js',
+  '/src/js/material.min.js',
   'https://fonts.googleapis.com/css?family=Roboto:400,700',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
-self.addEventListener('install', function (event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME)
-      .then(function (cache) {
-        console.log('[Service Worker] Precaching App Shell');
+      .then(function(cache) {
         cache.addAll(STATIC_FILES);
       })
   )
 });
 
-self.addEventListener('activate', function (event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys()
-      .then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key);
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME) {
             return caches.delete(key);
           }
         }));
       })
   );
-  return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
-  var url = 'https://httpbin.org/get';
+// Helper Function 驗證
+const isInArray = (string, array) => {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (element === string) return true;
+  }
+  return false;
+}
 
-  if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.indexOf(url) > -1 ) {
+    event.respondWith( // Network Only ##
       caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              cache.put(event.request, res.clone());
-              return res;
-            });
+        .then(async (cache) => {
+          const response = await fetch(event.request);
+          cache.put(event.request.url, response.clone());
+          return response;
         })
     );
-  } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
-    event.respondWith(
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    event.respondWith( // Cache Only ##
       caches.match(event.request)
     );
   } else {
-    event.respondWith(
+    event.respondWith( // Cache fallback Network ##
       caches.match(event.request)
-        .then(function (response) {
-          if (response) {
+        .then(async (existedCache) => {
+          if (existedCache) return existedCache;
+
+          try {
+            const response = await fetch(event.request);
+            const cache = await caches.open(CACHE_DYNAMIC_NAME);
+            cache.put(event.request.url, response.clone());
             return response;
-          } else {
-            return fetch(event.request)
-              .then(function (res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function (cache) {
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
-              })
-              .catch(function (err) {
-                return caches.open(CACHE_STATIC_NAME)
-                  .then(function (cache) {
-                    if (event.request.url.indexOf('/help')) {
-                      return cache.match('/offline.html');
-                    }
-                  });
-              });
+          } catch (error) {
+            const routingCache = await caches.open(CACHE_STATIC_NAME);
+            return routingCache.match('/index.html');
           }
         })
-    );
+    )
   }
 });
 
+// Network fallback to Cache 必須要等網路Timeout 才可以取用cache
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then(async (response) => {
+//         const cache = await caches.open(CACHE_DYNAMIC_NAME); // fetch 到資料同時也存進Cache
+//         cache.put(event.request.url, response.clone());
+//         return response;
+//       })
+//       .catch(() => caches.match(event.request))
+//   );
+// });
+
+// Cache Only##
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
 //     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;
-//         } else {
-//           return fetch(event.request)
-//             .then(function(res) {
-//               return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//             })
-//             .catch(function(err) {
-//               return caches.open(CACHE_STATIC_NAME)
-//                 .then(function(cache) {
-//                   return cache.match('/offline.html');
-//                 });
-//             });
+//   );
+// });
+
+// Network Only##
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then((response) => response)
+//   );
+// });
+
+// Cache fallback Network##
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request) // 查詢 Cache 是否存在
+//       .then(async (existedCache) => {
+//         if (existedCache) existedCache; // 是，優先取用 Cache
+
+//         try { // 否，則 fetch
+//           const response = await fetch(event.request);
+//           const cache = await caches.open(CACHE_DYNAMIC_NAME);
+//           cache.put(event.request.url, response.clone()); // 同時將 fetch 到的資料寫進 Cache
+//           return response; // 記得 return fetch 結果
+//         } catch (error) { // 若 fetch 失敗則 reutrn 靜態 cache 中的網頁。
+//           const routingcache = await caches.open(CACHE_STATIC_NAME);
+//           return routingcache.match('/offline.html');
 //         }
 //       })
-//   );
-// });
-
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     fetch(event.request)
-//       .then(function(res) {
-//         return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//       })
-//       .catch(function(err) {
-//         return caches.match(event.request);
-//       })
-//   );
-// });
-
-// Cache-only
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//   );
-// });
-
-// Network-only
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     fetch(event.request)
 //   );
 // });
